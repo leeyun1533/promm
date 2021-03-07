e158581452a8002a97661ae6ae8deda78a2c0d62
jeongyun.lee <jeongyun.lee@linecorp.com>
2021-03-07 13:54:03 +0900

    Fix the bug that sound only one active

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -53,16 +53,15 @@ function toggleTheme(theme) {
   }
 }
 
-let soundOn = undefined
-let soundOff = undefined
-if (typeof Audio != 'undefined') {
-  soundOn = new Audio(switchOnSound)
-  soundOff = new Audio(switchOffSound)
-}
-
 export const ThemeSwitch = () => {
   const [checked, setChecked] = useState(false)
 
+  let soundOn = undefined
+  let soundOff = undefined
+  if (typeof Audio != 'undefined') {
+    soundOn = new Audio(switchOnSound)
+    soundOff = new Audio(switchOffSound)
+  }
   const handleChange = checkedEvent => {
     const theme = getTheme(checkedEvent)
 
