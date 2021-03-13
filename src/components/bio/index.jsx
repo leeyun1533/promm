25183085e209de4a2ce8e0d4f83a5a8164616b73
jeongyun.lee <jeongyun.lee@linecorp.com>
2021-03-13 19:34:10 +0900

    fix the resume link name

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -24,9 +24,8 @@ export const Bio = () => (
                 }}
               />
               <div className="author-name">
-                <span className="author-name-prefix">Written by</span>
                 <Link to={'/about'} className="author-name-content">
-                  <span>@{author}</span>
+                  <span>Resume</span>
                 </Link>
                 <div className="author-introduction">{introduction}</div>
                 <p className="author-socials">
